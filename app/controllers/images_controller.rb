821132a990d7b20aec7ad690bfac39e421480c90
Cait Friedlander <caitfinchart@gmail.com>
2016-11-02 10:55:59 -0700

    add bootstrap navbar

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -5,7 +5,7 @@ class ImagesController < ApplicationController
 
   def create
     @board = Board.find(params[:id])
-    @image = @board.images.new
+    @image = @board.images.new(image_params)
     if @image.save
       redirect_to board_path @board.id
     else
@@ -16,4 +16,9 @@ class ImagesController < ApplicationController
   def show
     @image = Image.find(params[:id])
   end
+private
+    # Implement Strong Params
+    def image_params
+      params.require(:image).permit(:url, :board_id)
+    end
 end
